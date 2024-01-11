16d55c787f1d42d4274fe45a3ba891cb4bb7359c
anom35 <freelance35830@gmail.com>
2024-01-11 10:56:06 +0100

    Responsive

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from "../layout/Header"
 import Footer from "../components/Footer"
 import Shaping from "../layout/Shaping"
+import fond from "../assets/images/fond_erreur.webp"
 import "../styles/error404.css"
 
 export default function Error404() {
@@ -10,8 +11,10 @@ export default function Error404() {
             <Shaping>
                 <Header />
                 <div className="error404">
-                    <h1>404</h1>
-                    <h2>Page non trouvée</h2>
+                    <h1>Page non trouvée !</h1>
+                    <p>Veuillez cliquer sur le menu Accueil</p>
+                    <h2>ERREUR</h2>
+                    <img src={fond} alt="voiture peugeot 404" />
                 </div>
             </Shaping>
             <Footer />
--- src/pages/Reservations.jsx
+++ src/pages/Reservations.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import Header from "../layout/Header"
+import Shaping from '../layout/Shaping'
+import Footer from "../components/Footer"
 import "../styles/reservations.css"
 
 export default function Reservations() {
 
     return (
         <>
-            <Header />
-            <div className='reservations'>
-                <h1>Les réservations</h1>
-                <article>
-                    <p>Pour <strong>bénéficier d’un jardin familial</strong>, 
-                        il faut en faire la demande en <strong>completant et retournant le formulaire ci-dessous</strong>
-                        &nbsp;à l'adresse indiquée. Si aucune parcelle n'est disponible au moment de la demande, 
-                        le demandeur sera inscrit sur une <strong>liste d'attente</strong>.
-                    </p>
-                </article>
-                <a 
-                    className='btn'
-                    href="https://docs.google.com/forms/d/e/1FAIpQLScrgbwi_Txv3kSUeKnynqbMDPFDFPxQVpE2ff-HPmLsFWMl3w/viewform?entry.432530089=80+m2+%C3%A0+la+Piconnerie" 
-                    target="_blank" 
-                    rel="noopener noreferrer">
-                    Demande de jardin familial en ligne
-                </a>
-            </div>
+            <Shaping>
+                <Header />
+                <div className='reservations'>
+                    <h1>Les réservations</h1>
+                    <article>
+                        <p>Pour <strong>bénéficier d’un jardin familial</strong>, 
+                            il faut en faire la demande en <strong>completant et retournant le formulaire ci-dessous</strong>
+                            &nbsp;à l'adresse indiquée. Si aucune parcelle n'est disponible au moment de la demande, 
+                            le demandeur sera inscrit sur une <strong>liste d'attente</strong>.
+                        </p>
+                    </article>
+                    <a 
+                        className='btn'
+                        href="https://docs.google.com/forms/d/e/1FAIpQLScrgbwi_Txv3kSUeKnynqbMDPFDFPxQVpE2ff-HPmLsFWMl3w/viewform?entry.432530089=80+m2+%C3%A0+la+Piconnerie" 
+                        target="_blank" 
+                        rel="noopener noreferrer">
+                        Demande de jardin familial en ligne
+                    </a>
+                </div>
+            </Shaping>
+            <Footer />
         </>
     )
 }
