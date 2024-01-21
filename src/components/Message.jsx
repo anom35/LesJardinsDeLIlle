6e98b74d0ef74bf3463ecd90e50d1c8dd3506488
anom35 <freelance35830@gmail.com>
2024-01-21 19:50:02 +0100

    commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,45 @@
 import React, { useState, useEffect } from 'react';
 
-export default function MessageComponent({ message }) {
+export default function MessageComponent({ message, erreur }) {
     const [isVisible, setIsVisible] = useState(true);
+    const color = erreur ? "var(--bg-message-erreur)" : "var(--bg-message)";
 
     useEffect(() => {
         const timer = setTimeout(() => {
             setIsVisible(false);
-        }, 3000);
+        }, 5000);
 
         return () => {
             clearTimeout(timer);
         };
     }, []);
 
-  return isVisible ? (
-        <div style={{ backgroundColor: 'green', color: 'white', padding: '10px', borderRadius: '5px', textAlign: 'center' }}>
-            {message}
+    return isVisible ? (
+        <div style={{ 
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            height: '100vh',
+            width: '100vw',
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            zIndex: 1000
+        }}>
+            <div style={{ 
+                backgroundColor: color, 
+                color: 'black',
+                fontSize: '20px', 
+                padding: '20px', 
+                borderRadius: '10px', 
+                border: '2px solid white',
+                boxShadow: '0 0 20px black',
+                maxWidth: '80%', 
+                textAlign: 'center'
+            }}>
+                {message}
+            </div>
         </div>
     ) : null;
 };
--- src/pages/Administration.jsx
+++ src/pages/Administration.jsx
@@ -13,6 +13,9 @@ export default function Administration({ setIsLoggedIn }) {
     const navigate = useNavigate();
     
     const [allUsers, setAllUsers] = useState([]);
+    const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [btnCreateAdherent, setBtnCreateAdherent] = useState(false);
 
     const [Error, setError] = useState("");
     const [selectedDate, setSelectedDate] = useState(today);
@@ -53,10 +56,17 @@ export default function Administration({ setIsLoggedIn }) {
                     caution_rendu: cautionRendu 
                 })
             });
-            if (!response.ok) { throw new Error(`Erreur HTTP ${response.status}`); }
-            <Message message="Adhérent créé avec succès!" />
+            if (!response.ok) { 
+                throw new Error(`Erreur HTTP ${response.status}`); 
+            } else { 
+                setSuccessMessage("Adhérent créé avec succès !");
+                setErrorMessage(false);
+                resetForm();
+            }
         } catch (error) {
             setError("Erreur lors de la création d'une fiche adhérent");
+            setSuccessMessage("Erreur lors de la création !");
+            setErrorMessage(true);
         }
     };
 
@@ -76,31 +86,45 @@ export default function Administration({ setIsLoggedIn }) {
         }
     };
 
+
+    const resetForm = () => {
+        setSelectedDate(today);
+        setNom("");
+        setPrenom("");
+        setAdresse("");
+        setTelephone("");
+        setEmail("");
+        setMotDePasse("");
+        setCaution("");
+        setTypePaiement("");
+        setNumParcelle("");
+        setFinInscription("");
+        setCautionRendu("");
+    };
+
     const handleLogout = () => {
         localStorage.removeItem('authToken');
         setIsLoggedIn(false);
         navigate('/');
     };
     
+
+    const activeCreateAdherant = () => {
+        setBtnCreateAdherent(!btnCreateAdherent);
+    }
+
     useEffect(() => {
-
         getAllAdherants();
-
+        resetForm();
         const handleBeforeUnload = () => {
             localStorage.removeItem('authToken');
         };
         const handleRouteChange = () => {
             handleLogout();
         };
-
         window.addEventListener('beforeunload', handleBeforeUnload);
         return () => {
             window.removeEventListener('beforeunload', handleBeforeUnload);
-        };
-
-        const unlisten = navigate.listen(handleRouteChange);
-        return () => {
-            unlisten(); 
         };
     }, [navigate]);
 
@@ -110,63 +134,79 @@ export default function Administration({ setIsLoggedIn }) {
             <Shaping>
                 <Header />
                 <div className="fc fdc aic admin-container">
-                    {/* <h2>Page d'Administration</h2> */}
-                    <div className='ajout-jardinier'>
+                    <div className="boutons">
+                        <button className="btn2" onClick={activeCreateAdherant}>Création adhérent</button>
+                        <button className="btn2" onClick={activeCreateAdherant}>Liste des adhérents</button>
+                    </div>                
+                    <div className={btnCreateAdherent ? "ajout-jardinier show-on" : "ajout-jardinier show-off"}>
                         <h3 className='fc'>Créer une fiche adhérent</h3>
                         <form className="create-user" action="#" autoComplete="off">
-                            <label>Date d'inscription</label>
-                            <input
-                                type="date"
-                                value={selectedDate}
-                                onChange={(e) => setSelectedDate(e.target.value)}
-                            />
-                            <label>Nom</label>
-                            <input type="text" placeholder="Nom" value={nom} onChange={(e) => setNom(e.target.value)}/>
-                            <label>Prénom</label>
-                            <input type="text" placeholder="Prénom" value={prenom} onChange={(e) => setPrenom(e.target.value)} />
-                            <label>Adresse</label>
-                            <input type="text" placeholder="Adresse" value={adresse} onChange={(e) => setAdresse(e.target.value)}/>
-                            <label>Téléphone</label>
-                            <input type="text" placeholder="Téléphone" value={telephone} onChange={(e) => setTelephone(e.target.value)} />
-                            <label>Email</label>
-                            <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                            <label>Mot de passe (admin)</label>
-                            <input type="password" placeholder="<PASSWORD>"  value={motDePasse} onChange={(e) => setMotDePasse(e.target.value)} />
-                            <label className='choix-jardin'>Jardin :&nbsp;
-                                <select value={selectedJardin} onChange={(e) => setSelectedJardin(e.target.value)}>
-                                    <option value="Chaponerais">Chaperonnerais</option>
-                                    <option value="Piconnerie">Piconnerie</option>
-                                </select>
-                            </label>
-                            <label>Parcelle(s)</label>
-                            <input type="text" placeholder="18a" value={numParcelle} onChange={(e) => setNumParcelle(e.target.value)} />
-                            <label>Caution</label>
-                            <input type="text" placeholder="50€" value={caution} onChange={(e) => setCaution(e.target.value)} />
-                            <label>Type de paiement</label>
-                            <input type="text" placeholder="Type de paiement" value={typePaiement} onChange={(e) => setTypePaiement(e.target.value)}/>
+                            <div className="form-container">
+                                <div className="form1">
+                                    <label>Date d'inscription</label>
+                                    <input
+                                        type="date"
+                                        value={selectedDate}
+                                        onChange={(e) => setSelectedDate(e.target.value)}
+                                    />
+                                    <label>Nom</label>
+                                    <input type="text" placeholder="Nom" value={nom} onChange={(e) => setNom(e.target.value)}/>
+                                    <label>Prénom</label>
+                                    <input type="text" placeholder="Prénom" value={prenom} onChange={(e) => setPrenom(e.target.value)} />
+                                    <label>Adresse</label>
+                                    <input type="text" placeholder="Adresse" value={adresse} onChange={(e) => setAdresse(e.target.value)}/>
+                                    <label>Téléphone</label>
+                                    <input type="text" placeholder="Téléphone" value={telephone} onChange={(e) => setTelephone(e.target.value)} />
+                                    <label>Email</label>
+                                    <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                    <label>Mot de passe (admin)</label>
+                                    <input type="password" placeholder="<PASSWORD>"  value={motDePasse} onChange={(e) => setMotDePasse(e.target.value)} />
+                                </div>
+                                <div className="form2">
+                                    <label className='choix-jardin'>Jardin :&nbsp;
+                                        <select value={selectedJardin} onChange={(e) => setSelectedJardin(e.target.value)}>
+                                            <option value="Chaponerais">Chaperonnerais</option>
+                                            <option value="Piconnerie">Piconnerie</option>
+                                        </select>
+                                    </label>
+                                    <label>Parcelle(s)</label>
+                                    <input type="text" placeholder="18a" value={numParcelle} onChange={(e) => setNumParcelle(e.target.value)} />
+                                    <label>Caution</label>
+                                    <input type="text" placeholder="50€" value={caution} onChange={(e) => setCaution(e.target.value)} />
+                                    <label>Type de paiement</label>
+                                    <input type="text" placeholder="Type de paiement" value={typePaiement} onChange={(e) => setTypePaiement(e.target.value)}/>
 
-                            <label>Date de fin adhérent</label>
-                            <input
-                                type="date"
-                                value={fin_inscription}
-                                placeholder=''
-                                onChange={(e) => setFinInscription(e.target.value)}
-                            />
+                                    <label>Date de fin adhérent</label>
+                                    <input
+                                        type="date"
+                                        value={fin_inscription}
+                                        placeholder=''
+                                        onChange={(e) => setFinInscription(e.target.value)}
+                                    />
 
-                            <label>Caution rendu</label>
-                            <input type="text" placeholder="50€" value={cautionRendu} onChange={(e) => setCautionRendu(e.target.value)} />
+                                    <label>Caution rendu</label>
+                                    <input type="text" placeholder="50€" value={cautionRendu} onChange={(e) => setCautionRendu(e.target.value)} />
 
 
-                            <label>Date de rendu de la caution</label>
-                            <input
-                                type="date"
-                                value={cautionRendu}
-                                placeholder=''
-                                onChange={(e) => setCautionRendu(e.target.value)}
-                            />
-
+                                    <label>Date de rendu de la caution</label>
+                                    <input
+                                        type="date"
+                                        value={cautionRendu}
+                                        placeholder=''
+                                        onChange={(e) => setCautionRendu(e.target.value)}
+                                    />
+                                </div>
+                            </div>
 
                             <button type="button" onClick={createAdherant}>Créer adhérent</button>
+
+                            {!errorMessage && (
+                                (successMessage && <Message message={successMessage} erreur={false} />)
+                            )}
+
+                            {errorMessage && (
+                                (successMessage && <Message message={successMessage} erreur={true} />)
+                            )}
                         </form>
                     </div>
                 </div>
